2be5a904d70a89ad1453b95bf805edd9dd9a1c60
Carlo Lanzi Luciani <carlo.lanzi.luciani@intra2net.com>
2025-08-13 09:48:10 +0200

    Add exercise to equip PDF with trivial Outline (ToC)
    This exercise is executed in two different ways:
    
    1. Manually building the Outlines tree with the low level
    instruments provided by lopdf.
    
    2. Using the `add_bookmark()` and `build_outline()` functions
    provided by lopdf. Their usage is absolutely not obvious:
    
    	1. `add_bookmark()` updates the bookmark, max_id and
    	bookmarks_table of the Document struct, but does not
    	actually build the tree.
    
    	2. `build_outline()` does build all the tree of outline
    	items, but does not link it to the catalog with type
    	"Outlines" or does not set the "PageMode" to "UseOutlines":
    	both these must be manually done afterwards.
    
    This was a very good beginning to learn how to set the Outlines on
    PDF documents with the library provided. Now it is time to develop
    a real command line instrument with this knowledge.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,17 @@ enum DevCommands {
         #[arg(short, long, default_value_t = 2)]
         pages: u8,
     },
+    /// Equip an already existing PDF file with a trivial ToC (each page is bookmarked in the Outline)
+    EquipWithTrivialToc {
+        /// Document path
+        #[arg(short, long)]
+        doc_path: String,
+    },
+    Experiment {
+        /// Document path
+        #[arg(short, long)]
+        doc_path: String,
+    },
 }
 
 fn main() -> Result<()> {
@@ -34,6 +45,12 @@ fn main() -> Result<()> {
             DevCommands::GenerateBasicPDF { name, pages } => {
                 dev_exercises::generate_basic_pdf(&name, pages)?;
             }
+            DevCommands::EquipWithTrivialToc { doc_path } => {
+                dev_exercises::equip_with_trivial_toc(&doc_path)?;
+            }
+            DevCommands::Experiment { doc_path } => {
+                dev_exercises::experiment_to_equip_trivial_doc(&doc_path)?;
+            }
         }
     }
 
@@ -41,12 +58,202 @@ fn main() -> Result<()> {
 }
 
 mod dev_exercises {
-    use anyhow::Result;
+    use anyhow::{Result, anyhow};
     use lopdf::{
-        self, Document, Object, ObjectId, Stream,
+        self, Bookmark, Dictionary, Document, Object, ObjectId, Outline, Stream,
         content::{Content, Operation},
         dictionary,
     };
+    use std::{collections::BTreeMap, path::Path};
+
+    const DEV_PLAYGROUND_DIR: &str = "dev-playground";
+
+    pub fn experiment_to_equip_trivial_doc(doc_path: &str) -> Result<()> {
+        let mut doc = Document::load(doc_path)?;
+
+        match doc.get_toc() {
+            Ok(_toc) => return Err(anyhow!("A ToC is already present on such document")),
+            Err(_no_outline) => (),
+        }
+
+        let outlines_id = doc.new_object_id();
+
+        let outline_items_ids: Vec<_> = doc
+            .get_pages()
+            .iter()
+            .map(|(page_numer, page_id)| {
+                doc.add_bookmark(
+                    Bookmark::new("Page".to_string(), [0f32; 3], 0, *page_id),
+                    None,
+                )
+            })
+            .collect();
+
+        let outlines_id = doc
+            .build_outline()
+            .ok_or(anyhow!("The doc bookmarks table could not be built"))?;
+        let catalog = doc.catalog_mut()?;
+
+        catalog.set("Outlines", Object::Reference(outlines_id));
+        catalog.set(
+            "PageMode",
+            Object::String("UseOutlines".into(), lopdf::StringFormat::Literal),
+        );
+
+        let doc_name = Path::new(doc_path)
+            .file_name()
+            .ok_or(anyhow!("The path provided does not point to a file"))?
+            .to_str()
+            .ok_or(anyhow!(
+                "The path provided contains non-UTF8 chars (not supported)"
+            ))?
+            .to_string();
+
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let parent_folder = format!("{manifest_dir}/{DEV_PLAYGROUND_DIR}/equip_with_trivial_toc");
+        std::fs::create_dir_all(&parent_folder)?;
+
+        let doc_name = format!("{}-with-outline.pdf", doc_name);
+        let filepath = format!("{parent_folder}/{doc_name}");
+        doc.save(filepath)?;
+
+        Ok(())
+    }
+
+    pub fn equip_with_trivial_toc(doc_path: &str) -> Result<()> {
+        let mut doc = Document::load(doc_path)?;
+
+        match doc.get_toc() {
+            Ok(_toc) => return Err(anyhow!("A ToC is already present on such document")),
+            Err(_no_outline) => (),
+        }
+
+        // todo: add verification for PDF metadata on the document, and return error if they
+        // are present, because they will not be copied to the new document
+
+        // objects may have an id: (object_number, object_generation)
+
+        let outlines_id = doc.new_object_id();
+
+        // Outlines is directly a child of the catalog (=root) with such 4 entries:
+        // type Outlines
+        // First and Last: the references to the linked list of the first and last outline items (thus referencing the object ids of such)
+        // count the total number of entries in the whole outlines tree
+
+        // outlines contains outline-items
+        // at each level of the hierarchy the outline-items are a linked list. One can access teh beginning or end of hte linked list
+        // from his parent with the keyword first and last
+
+        // the outline items are structured so:
+        // title String
+        // parent: indirect reference to the parent
+        // Prev - Next : the indirect references to the previous and next level in the list of same level outline items
+        // First - Last : as for outlines, the references of the linked list below, if present. Otherwise not required
+        // Count: count all the descendants, if any. Otherwise not required
+        // Dest: name, byte string or array to specify a destination
+
+        // what is a destination?
+        // what I need in my case is this syntax:
+        // [ page /Fit], where page is a reference to a page, ie the page id
+        // or eventually this one
+        // [ page /XYZ left top zoom], with left top zoom coords which can be set to 0/null
+
+        // it is not clear if I should also use the option PageMode set to UseOutlines as a value in the catalog node
+
+        let pages_ids: Vec<_> = doc
+            .get_pages()
+            .iter()
+            .map(|(_page_number, page_id)| page_id)
+            .cloned()
+            .collect();
+
+        let proto_outlines: Vec<_> = pages_ids
+            .iter()
+            .map(|page_id| (page_id, doc.new_object_id()))
+            .collect();
+
+        let first_outline_id = proto_outlines
+            .first()
+            .ok_or(anyhow!("The proto oulines vector has no memebers!"))?
+            .1;
+        let last_outline_id = proto_outlines
+            .last()
+            .ok_or(anyhow!("The proto oulines vector has no memebers!"))?
+            .1;
+
+        for (index, proto_outline) in proto_outlines.iter().enumerate() {
+            let (page_id, outline_item_id) = proto_outline;
+
+            let prev = if index == 0 {
+                None
+            } else {
+                proto_outlines
+                    .get(index - 1)
+                    .map(|(_page_id, outline_id)| outline_id)
+            };
+
+            let next = if index == proto_outlines.len() {
+                None
+            } else {
+                proto_outlines
+                    .get(index + 1)
+                    .map(|(_page_id, outline_id)| outline_id)
+            };
+
+            let outline_item = dictionary! {
+                "Title" => Object::String(format!("Page {}", index + 1).as_bytes().to_vec(), lopdf::StringFormat::Literal),
+                "Parent" => Object::Reference(outlines_id),
+                "Prev" => match prev {
+                    None => Object::Null,
+                    Some(prev) => Object::Reference(*prev),
+                },
+                "Next" => match next {
+                    None => Object::Null,
+                    Some(next) => Object::Reference(*next),
+                },
+                "Dest" => Object::Array(vec![Object::Reference(**page_id), "XYZ".into(), Object::Null,Object::Null,Object::Null,]),
+
+            };
+
+            doc.set_object(*outline_item_id, outline_item);
+        }
+
+        let outlines = dictionary! {
+            "Type" => Object::Name("Outlines".as_bytes().to_vec()),
+            "First" => Object::Reference(first_outline_id),
+            "Last" => Object::Reference(last_outline_id),
+            "Count" => Object::Integer(proto_outlines.len() as i64),
+        };
+
+        doc.set_object(outlines_id, outlines);
+
+        let catalog = doc.catalog_mut()?;
+
+        catalog.set("Outlines", Object::Reference(outlines_id));
+        catalog.set(
+            "PageMode",
+            Object::String("UseOutlines".into(), lopdf::StringFormat::Literal),
+        );
+
+        let doc_name = Path::new(doc_path)
+            .file_name()
+            .ok_or(anyhow!("The path provided does not point to a file"))?
+            .to_str()
+            .ok_or(anyhow!(
+                "The path provided contains non-UTF8 chars (not supported)"
+            ))?
+            .to_string();
+
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
+        let parent_folder = format!("{manifest_dir}/{DEV_PLAYGROUND_DIR}/equip_with_trivial_toc");
+        std::fs::create_dir_all(&parent_folder)?;
+
+        let doc_name = format!("{}-with-outline.pdf", doc_name);
+        let filepath = format!("{parent_folder}/{doc_name}");
+        doc.save(filepath)?;
+
+        Ok(())
+    }
 
     /// Generate a PDF file with minimal features
     pub fn generate_basic_pdf(doc_name: &str, pages: u8) -> Result<()> {
@@ -95,7 +302,7 @@ mod dev_exercises {
         doc.compress();
 
         let manifest_dir = env!("CARGO_MANIFEST_DIR");
-        let parent_folder = format!("{manifest_dir}/{DEV_DATA_FOLDER}/generate_basic_pdf");
+        let parent_folder = format!("{manifest_dir}/{DEV_PLAYGROUND_DIR}/generate_basic_pdf");
         std::fs::create_dir_all(&parent_folder)?;
 
         let doc_name = if doc_name.ends_with(".pdf") {
@@ -111,12 +318,7 @@ mod dev_exercises {
         Ok(())
     }
 
-    fn append_random_page_to_doc(
-        page_number: u8,
-        doc_name: &str,
-        pages_id: &ObjectId,
-        doc: &mut Document,
-    ) -> Result<ObjectId> {
+    fn append_random_page_to_doc(page_number: u8, doc_name: &str, pages_id: &ObjectId, doc: &mut Document) -> Result<ObjectId> {
         let page_title = format!("Page {page_number}");
         let random_text = craft_random_text_of_len(20);
 
