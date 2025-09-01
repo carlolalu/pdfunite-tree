e8d7484e666e88a477f6244a99bd26744624577b
carlolalu <carlolanziluciani@gmail.com>
2025-09-01 23:04:08 +0200

    Add functionality to retrieve catalog's children name to PDF tool

--- src/bin/pdf-my-tool.rs
+++ src/bin/pdf-my-tool.rs
@@ -1,6 +1,7 @@
 use anyhow::{Result, anyhow};
 use clap::{Parser, Subcommand};
-use pdfunite_tree::utils::get_basic_pdf_doc;
+use lopdf::Document;
+use pdfunite_tree::utils::{get_basic_pdf_doc, get_catalog_children_names};
 use std::path::Path;
 
 /// Generate a PDF document with random content. The pages have for title the name of the document and the page number.
@@ -23,6 +24,11 @@ enum ToolCmd {
         #[arg(short = 'n')]
         num_pages: u8,
     },
+    /// Show the names of all the children of the Catalog's PDF
+    ShowCatalogChildren {
+        /// Path of the pdf file
+        pdf_path: String,
+    },
 }
 
 fn main() {
@@ -33,6 +39,7 @@ fn main() {
             output_path,
             num_pages,
         } => generate_basic_pdf_doc(output_path, num_pages),
+        ToolCmd::ShowCatalogChildren { pdf_path } => show_catalog_children_names(pdf_path),
     };
 
     if let Err(err) = result {
@@ -62,6 +69,26 @@ fn generate_basic_pdf_doc(output_path: impl AsRef<Path>, num_pages: u8) -> Resul
     let mut buffer = Vec::new();
     random_doc.save_modern(&mut buffer)?;
     std::fs::write(output_path, buffer)?;
+
+    Ok(())
+}
+
+fn show_catalog_children_names(input_path: impl AsRef<Path>) -> Result<()> {
+    let input_path = input_path.as_ref();
+
+    let catalog_children_names = get_catalog_children_names(&Document::load(input_path)?)?;
+
+    println!("Catalog");
+    let num_children = catalog_children_names.len();
+
+    for (index, child) in catalog_children_names.iter().enumerate() {
+        let prefix = if index < num_children - 1 {
+            "├── ".to_string()
+        } else {
+            "└── ".to_string()
+        };
+        println!("{prefix}{child}");
+    }
 
     Ok(())
 }
